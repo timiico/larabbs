8ffbe90211ff9947fa4532094de0a29c705c8a9c
timiico <2862291487@qq.com>
2020-12-22 03:00:50 +0000

    分类话题列表

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -10,7 +10,7 @@ class Topic extends Model
 
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
 
-    public function categroy()
+    public function category()
     {
         return $this->belongsTo(Category::class);
     }
--- app/helpers.php
+++ app/helpers.php
@@ -3,3 +3,8 @@ function root_class()
 {
     return str_replace('.','-',Route::currentRouteName());
 }
+
+function category_nav_active($category_id)
+{
+    return active_class((if_route('category.show') && if_route_param('category',$category_id)));
+}
